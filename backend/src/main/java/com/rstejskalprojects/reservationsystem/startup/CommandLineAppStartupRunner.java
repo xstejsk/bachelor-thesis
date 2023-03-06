c08d0f108d9295c79e6a16c84c2d32a51020c5a1
Radim Stejskal <xstejsk@fi.muni.cz>
2023-03-06 22:05:02 +0100

    fix: init database population now shoul not throw an error

--- backend/src/main/java/com/rstejskalprojects/reservationsystem/startup/CommandLineAppStartupRunner.java
+++ backend/src/main/java/com/rstejskalprojects/reservationsystem/startup/CommandLineAppStartupRunner.java
@@ -28,26 +28,30 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     @Override
     public void run(String...args){
         try {
-            Location location = locationRepository.save(new Location("Sal1", "nejvetsi sal"));
-            Location location2 = locationRepository.save(new Location("Sal2", "mensi sal"));
-            Event event = new Event(LocalDateTime.now().minusHours(2), LocalDateTime.now().plusMinutes(80),
-                    5, 100d, "Event v Salu 1", "popis",  null, locationRepository.findById(location.getId()).get());
-            Event event1 = new Event(LocalDateTime.now().minusHours(3), LocalDateTime.now().minusHours(1),
-                    5, 100d, "Event v Salu 2", "popis",  null, locationRepository.findById(location2.getId()).get());
-            eventRepository.save(event);
-            eventRepository.save(event1);
+            Location location = new Location("Sal1", "nejvetsi sal");
+            Location location2 = new Location("Sal2", "mensi sal");
+            if (locationRepository.findAll().isEmpty()) {
+                locationRepository.save(location);
+                locationRepository.save(location2);
 
+                Event event = new Event(LocalDateTime.now().minusHours(2), LocalDateTime.now().plusMinutes(80),
+                        5, 100d, "Event v Salu 1", "popis",  null, locationRepository.findById(location.getId()).get());
+                Event event1 = new Event(LocalDateTime.now().minusHours(3), LocalDateTime.now().minusHours(1),
+                        5, 100d, "Event v Salu 2", "popis",  null, locationRepository.findById(location2.getId()).get());
+                if (eventRepository.findAll().isEmpty()) {
+                    eventRepository.save(event);
+                    eventRepository.save(event1);
+                }
+                String password = "admin";
+                String encodedPw = bCryptPasswordEncoder.encode(password);
+                AppUser admin = new AppUser(1L, "Vlastimil", "Novák", "admin", "admin", encodedPw, UserRoleEnum.ADMIN, false, true);
+                AppUser user = new AppUser(2L, "Petr", "Jirák", "user", "user", bCryptPasswordEncoder.encode("user"), UserRoleEnum.USER, false, true);
+                if (userRepository.findAll().isEmpty()) {
+                    userRepository.save(admin);
+                    userRepository.save(user);
+                }
+            }
 
-            String password = "admin";
-            String encodedPw = bCryptPasswordEncoder.encode(password);
-            RecurrenceGroup recurrenceGroup = null;
-            //recurrenceGroupRepository.save(recurrenceGroup);
-            //RecurrenceGroup recurrenceGroup1 = recurrenceGroupRepository.findById(1L).get();
-
-            AppUser admin = new AppUser(1L, "Vlastimil", "Novák", "admin", "admin", encodedPw, UserRoleEnum.ADMIN, false, true);
-            AppUser user = new AppUser(2L, "Petr", "Jirák", "user", "user", bCryptPasswordEncoder.encode("user"), UserRoleEnum.USER, false, true);
-            userRepository.save(admin);
-            userRepository.save(user);
         } catch (Exception e){
          //   e.printStackTrace();
         }
